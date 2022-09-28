f32f3d0fcfb2abf7b7883d5a99aa6c4d8d60c357
Prateek Gupta <prateek@wolkensoftware.com>
2022-09-28 19:04:20 +0530

    Updated Repository

--- Document Generator/src/main/java/com/prateekgupta/DocumentGenerator/repository/Repository.java
+++ Document Generator/src/main/java/com/prateekgupta/DocumentGenerator/repository/Repository.java
@@ -2,11 +2,17 @@ package com.prateekgupta.DocumentGenerator.repository;
 
 import com.prateekgupta.DocumentGenerator.entities.DocumentMaster;
 import com.prateekgupta.DocumentGenerator.entities.HTMLContentMaster;
+import com.prateekgupta.DocumentGenerator.entities.TabularContentMaster;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface Repository extends JpaRepository<DocumentMaster,Integer> {
     @Query("select hcm from DocumentMaster dm, HTMLContentMaster hcm, DocumentHTMLContentMapping dhm  where dm.documentId=dhm.documentMaster.documentId and dhm.htmlContentMaster.htmlContentId=hcm.htmlContentId and dm.documentId=:documentId")
     HTMLContentMaster getHTMLContent(@Param("documentId") Integer documentId);
+
+    @Query("select tcm from DocumentMaster dm, TabularContentMaster tcm, DocumentTabularContentMapping dtcm where dm.documentId=dtcm.documentMaster.documentId and dtcm.tabularContentMaster.tabularContentId=tcm.tabularContentId and dm.documentId=:documentId")
+    List<TabularContentMaster> getTabularContent(@Param("documentId") Integer documentId);
 }
