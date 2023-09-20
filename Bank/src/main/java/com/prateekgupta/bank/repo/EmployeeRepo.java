03cd98dd7b881725f63278ce662e5cdf2c9d2380
Prateek Gupta <prateek@wolkensoftware.com>
2023-09-20 18:43:08 +0530

    Added logs for Sample Project

--- Bank/src/main/java/com/prateekgupta/bank/repo/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.prateekgupta.bank.repo;
-
-import com.prateekgupta.bank.entity.EmployeeEntity;
-import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
-
-public interface EmployeeRepo extends JpaRepositoryImplementation<EmployeeEntity,Integer> {
-    EmployeeEntity findByDesignation(String designation);
-    EmployeeEntity findBySalary(float salary);
-}
--- Sample Project/src/main/java/prateek_gupta/sample_project/core/service/impl/CoreServiceImpl.java
+++ Sample Project/src/main/java/prateek_gupta/sample_project/core/service/impl/CoreServiceImpl.java
@@ -1,5 +1,7 @@
 package prateek_gupta.sample_project.core.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import prateek_gupta.sample_project.core.dao.Table1Repository;
@@ -10,19 +12,21 @@ import prateek_gupta.sample_project.core.vo.Table1VO;
 @Service
 public class CoreServiceImpl implements CoreService {
 
+    private final Logger log = LoggerFactory.getLogger(CoreServiceImpl.class);
+
     @Autowired
     Table1Repository table1Repository;
 
     @Override
     public Table1VO getTable1Details(Integer primaryKey) {
-
+        log.info("getTable1Details started");
         try{
             Table1Entity entity= table1Repository.findByPrimaryKey(primaryKey);
             return entity.toVO();
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
-
+        log.info("getTable1Details ended");
         return null;
     }
 }
