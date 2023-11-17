ceda99c0d80ec86be1e87440b5070322b5a7c195
Prateek Gupta <prateek@wolkensoftware.com>
2023-11-17 15:18:27 +0530

    Added Controller and basic get api for Spring MVC

---
+++ Spring MVC/src/main/java/Controller.java
@@ -0,0 +1,16 @@
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+
+    @RequestMapping(value = "save", method = RequestMethod.GET)
+    ModelAndView save(){
+        return new ModelAndView("save.html");
+    }
+
+
+}
