eea87e6f1172df1d9da058832f1b9b79d6dad755
Prateek Gupta <prateek@wolkensoftware.com>
2025-06-05 15:44:19 +0530

    Completed LogManager

--- Spring Boot/SampleProject/src/main/java/prateek_gupta/SampleProject/core/CoreController.java
+++ Spring Boot/SampleProject/src/main/java/prateek_gupta/SampleProject/core/CoreController.java
@@ -1,20 +1,25 @@
 package prateek_gupta.SampleProject.core;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import prateek_gupta.SampleProject.prateek_gupta.ServiceException;
 import prateek_gupta.SampleProject.utils.Util;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 public class CoreController {
 
-    private static final Logger logger = LogManager.getLogger(CoreController.class);
+    private static final Logger logger = LogManager.getLogger(
+            CoreController.class);
 
     @Autowired
     CoreService coreService;
@@ -30,6 +35,29 @@ public class CoreController {
         } catch (ServiceException exception) {
             return Util.getErrorResponse(new ServiceException());
         }
+        logger.info("Exiting test() Controller");
+        return response;
+    }
+
+    @PostMapping("rotate_log_files")
+    ResponseEntity<ObjectNode> rotateLogFiles(HttpServletRequest request) {
+        logger.info("Entering rotateLogFiles() Controller");
+        ResponseEntity<ObjectNode> response;
+        try {
+            String daysGapStr=request.getParameter("days_gap");
+            int daysGap;
+            if (StringUtils.isNotBlank(daysGapStr))
+                daysGap=Integer.parseInt(daysGapStr);
+            else
+                daysGap= prateek_gupta.SampleProject.prateek_gupta.LogManager.
+                        DAYS_GAP_FOR_ROTATION;
+            prateek_gupta.SampleProject.prateek_gupta.LogManager.rotateLogFiles(daysGap);
+            response = Util.getSuccessResponse("Successfully deleted the log files");
+
+        } catch (ServiceException exception) {
+            return Util.getErrorResponse(new ServiceException());
+        }
+        logger.info("Exiting rotateLogFiles() Controller");
         return response;
     }
 }
--- Spring Boot/SampleProject/src/main/java/prateek_gupta/SampleProject/prateek_gupta/LogManager.java
+++ Spring Boot/SampleProject/src/main/java/prateek_gupta/SampleProject/prateek_gupta/LogManager.java
@@ -8,19 +8,23 @@ import org.apache.logging.log4j.core.config.builder.impl.BuiltConfiguration;
 import org.apache.logging.log4j.core.config.plugins.Plugin;
 
 import java.io.File;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 
-@Plugin(name = "CustomConfigurationFactory",
+@Plugin(name = "LogManager",
         category = ConfigurationFactory.CATEGORY)
 @Order(50)
 public class LogManager extends ConfigurationFactory{
 
+    private static final Logger logger =
+            org.apache.logging.log4j.LogManager.getLogger(LogManager.class);
+
     private static final String LOG_DIR = "logs";
     private static final String LOG_FILE_PREFIX = "logs.";
     private static final String LOG_FILE_SUFFIX = ".log";
-    private static final int DAYS_GAP_FOR_ROTATION = 30;
+    public static final int DAYS_GAP_FOR_ROTATION = 30;
 
     @Override
     public String[] getSupportedTypes() {
@@ -30,11 +34,6 @@ public class LogManager extends ConfigurationFactory{
     @Override
     public Configuration getConfiguration(
             LoggerContext loggerContext, ConfigurationSource source) {
-        return LogManager.getBuiltConfiguration(true);
-    }
-
-    public static BuiltConfiguration getBuiltConfiguration(
-            boolean console_logs) {
         ConfigurationBuilder<BuiltConfiguration> configurationBuilder =
                 ConfigurationBuilderFactory.newConfigurationBuilder();
 
@@ -45,7 +44,7 @@ public class LogManager extends ConfigurationFactory{
         LayoutComponentBuilder layoutBuilder =
                 configurationBuilder.newLayout("PatternLayout")
                         .addAttribute("pattern",
-                                "%d{yyyy-MM-dd HH:mm:ss.SSS} %M(){%F} :: %msg%n");
+                                "%d{yyyy-MM-dd HH:mm:ss.SSS} %M(){%F} : %msg%n");
 
         // Log file rollover triggering Policies
         ComponentBuilder<?> triggeringPolicies =
@@ -57,15 +56,17 @@ public class LogManager extends ConfigurationFactory{
         RootLoggerComponentBuilder rootLoggerComponentBuilder =
                 configurationBuilder.newRootLogger(org.apache.logging.log4j.Level.INFO);
 
-        if (console_logs) {
+        if (ProjectSettings.console_logs) {
             // Console Appender
             AppenderComponentBuilder consoleAppender =
-                    configurationBuilder.newAppender("LogConsole", "Console")
+                    configurationBuilder.newAppender(
+                                    "LogConsole", "Console")
                             .addAttribute("target", "SYSTEM_OUT")
                             .add(layoutBuilder);
 
             configurationBuilder.add(consoleAppender);
-            rootLoggerComponentBuilder.add(configurationBuilder.newAppenderRef("LogConsole"));
+            rootLoggerComponentBuilder.add(
+                    configurationBuilder.newAppenderRef("LogConsole"));
         } else {
             // RollingFile Appender
             AppenderComponentBuilder rollingFileAppender =
@@ -81,62 +82,53 @@ public class LogManager extends ConfigurationFactory{
             rootLoggerComponentBuilder.add(configurationBuilder.newAppenderRef("LogFiles"));
         }
 
-
         // Root Logger
         configurationBuilder.add(rootLoggerComponentBuilder);
         return configurationBuilder.build();
     }
 
-    public static void rotateLogFiles() {
-        //logger.info("Log files rotation begins");
+    public static void rotateLogFiles(Integer daysGap)
+            throws ServiceException {
+        logger.info("Log files rotation begins");
         try {
             File logDirectory = new File(LOG_DIR);
-            if (!logDirectory.exists()) {
-                //logger.warn("Log directory not found.");
-                return;
-            }
+            if (!logDirectory.exists())
+                throw new ServiceException("Log directory not found");
 
             File[] files = logDirectory.listFiles(
-                    (dir, name) -> name.startsWith(LOG_FILE_PREFIX) &&
-                            name.endsWith(LOG_FILE_SUFFIX));
+                    (file) -> file.getName().startsWith(LOG_FILE_PREFIX) &&
+                            file.getName().endsWith(LOG_FILE_SUFFIX));
             if (files == null || files.length == 0) return;
 
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            SimpleDateFormat dateFormat=new SimpleDateFormat("yyyy-MM-dd");
+            Calendar calendar=Calendar.getInstance();
 
-            Arrays.stream(files).forEach(file -> {
+
+            Arrays.asList(files).forEach(file -> {
                 try {
-                    // logs.2024-05-01.1.log → extract 2024-05-01
                     String name = file.getName();
                     String[] parts = name.split("\\.");
                     if (parts.length >= 3) {
-                        LocalDate fileDate = LocalDate.parse(parts[1], formatter);
-                        if (fileDate.plusDays(DAYS_GAP_FOR_ROTATION).
-                                isBefore(LocalDate.now())) {
+                        Date date=dateFormat.parse(parts[1]);
+                        calendar.setTime(date);
+                        calendar.add(Calendar.DAY_OF_MONTH,daysGap);
+
+                        if (calendar.before(Calendar.getInstance())) {
                             if (file.delete()) {
-                                //logger.info("Deleted log file: {}", name);
+                                logger.info("Deleted log file: {}", name);
                             } else {
-                                //logger.warn("Failed to delete log file: {}", name);
+                                logger.warn("Failed to delete log file: {}", name);
                             }
                         }
                     }
                 } catch (Exception e) {
-//                    logger.error("Error parsing log file name: {}",
-//                            file.getName(), e);
+                    logger.error("Error parsing log file name: {}",
+                            file.getName(), e);
                 }
             });
         } catch (Exception e) {
-            //logger.error("Error while rotating log files", e);
+            throw new ServiceException("Error while rotating log files");
         }
-        //logger.info("Log files rotation ends");
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Starts");
-        LoggerContext ctx = Configurator.initialize(getBuiltConfiguration(true));
-        ctx.updateLoggers();
-        Logger logger =
-                org.apache.logging.log4j.LogManager.getLogger(LogManager.class);
-        logger.info("Logger initialized.");
-        rotateLogFiles();
+        logger.info("Log files rotation ends");
     }
 }
