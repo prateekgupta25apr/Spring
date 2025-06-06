761f227592db969bd5499d0888366e093bb50140
Prateek Gupta <prateek@wolkensoftware.com>
2025-06-06 15:46:01 +0530

    Redirected JUL logs to SLF4j and  SLF4J logs to Log4J

--- Spring Boot/SampleProject/src/main/java/prateek_gupta/SampleProject/SampleProjectApplication.java
+++ Spring Boot/SampleProject/src/main/java/prateek_gupta/SampleProject/SampleProjectApplication.java
@@ -1,8 +1,13 @@
 package prateek_gupta.SampleProject;
 
+import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+
+import javax.annotation.PostConstruct;
+
+import java.util.logging.LogManager;
 
 import static prateek_gupta.SampleProject.prateek_gupta.ProjectSettings.
 		configuration_properties_file_path;
@@ -14,6 +19,13 @@ public class SampleProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SampleProjectApplication.class, args);
 		System.out.println("Good to go");
+	}
+
+	@PostConstruct
+	public void onLoad() {
+		LogManager.getLogManager().reset();
+		SLF4JBridgeHandler.removeHandlersForRootLogger();
+		SLF4JBridgeHandler.install();
 	}
 
 }
