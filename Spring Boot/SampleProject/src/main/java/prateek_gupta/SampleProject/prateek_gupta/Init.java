e813963c442049f6c60200e531409a67081a2c2c
Prateek Gupta <prateek.gupta25apr@gmail.com>
2025-06-07 18:18:20 +0530

    Added Init class

---
+++ Spring Boot/SampleProject/src/main/java/prateek_gupta/SampleProject/prateek_gupta/Init.java
@@ -0,0 +1,12 @@
+package prateek_gupta.SampleProject.prateek_gupta;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Init {
+    public static Map<String,Object> configuration_properties=new HashMap<>();
+
+    public static void onLoad(){
+
+    }
+}
